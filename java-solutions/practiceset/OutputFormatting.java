18cf2734b14292879d0350818ac55544a5151735
Sonal Priya <sonalpriyassm@gmail.com>
2021-01-30 22:58:22 +0530

    Problem 4

---
+++ java-solutions/practiceset/OutputFormatting.java
@@ -0,0 +1,21 @@
+package practiceset;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("================================");
+		for (int i = 0; i < 3; i++) {
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			System.out.printf("%-15s%03d\n", s1, x);
+			// Complete this line
+		}
+		System.out.println("================================");
+		sc.close();
+	}
+
+}
