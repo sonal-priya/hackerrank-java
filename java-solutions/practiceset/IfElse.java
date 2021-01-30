3abeaf3e7ad470c30a3ff8f76caf9ad53dbb0355
Sonal Priya <sonalpriyassm@gmail.com>
2021-01-30 22:55:48 +0530

    Problem 3

---
+++ java-solutions/practiceset/IfElse.java
@@ -0,0 +1,29 @@
+package practiceset;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String ans = "";
+		if (n % 2 == 1) {
+			ans = "Weird";
+		} else {
+
+			if (2 <= n && n <= 5) {
+				ans = "Not Weird";
+			} else if (6 <= n && n <= 20) {
+				ans = "Weird";
+			} else if (n > 20) {
+				ans = "Not Weird";
+			}
+
+		}
+		System.out.println(ans);
+		sc.close();
+	}
+
+}
