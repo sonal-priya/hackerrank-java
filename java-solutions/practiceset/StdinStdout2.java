993835aa8e0e3e17dc10d1c07750b7bfb89b6ede
Sonal Priya <sonalpriyassm@gmail.com>
2021-01-30 22:56:43 +0530

    Problem 4

---
+++ java-solutions/practiceset/StdinStdout2.java
@@ -0,0 +1,23 @@
+package practiceset;
+
+import java.util.Scanner;
+
+public class StdinStdout2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		double d = scan.nextDouble();
+		scan.nextLine();
+		String s = scan.nextLine();
+		scan.close();
+
+		// Write your code here.
+
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+	}
+
+}
